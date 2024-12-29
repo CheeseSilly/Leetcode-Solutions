1f77e11df1987dfd82460d333fdfbb185cd9fb65
Si11ycheese <si11ycheese6680@gmail.com>
2024-12-29 11:19:45 +0800

    update

---
+++ 797.所有可能的路径.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<std::vector<int>> res;
+  std::vector<std::vector<int>>
+  allPathsSourceTarget(std::vector<std::vector<int>> &graph) {
+    int n = graph.size();
+    std::vector<int> pass;
+    pass.push_back(0);
+    dfs(graph, pass, 0, n);
+    return res;
+  }
+
+private:
+  void dfs(std::vector<std::vector<int>> &graph, std::vector<int> &pass, int i,
+           int n) {
+    if (i == n - 1) {
+      res.push_back(pass);
+      return;
+    } else {
+      for (int t = 0; t < graph[i].size(); t++) {
+        pass.push_back(graph[i][t]);
+        dfs(graph, pass, graph[i][t], n);
+        pass.pop_back();
+      }
+    }
+  }
+};
