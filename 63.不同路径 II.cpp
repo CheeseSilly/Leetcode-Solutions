4e64a7c8016f565660b10bd52b3ae06c978fba2b
Si11ycheese <si11ycheese6680@gmail.com>
2025-02-08 01:01:13 +0800

    update

---
+++ 63.不同路径 II.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+  int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+    vector<vector<int>> moves = {{0, 1}, {1, 0}};
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+    vector<vector<int>> visited(m, vector<int>(n, -1));
+    int ans = 0;
+    auto dfs = [&](this auto &&dfs, int i, int j) -> int {
+      if (i < 0 || i >= m || j < 0 || j >= n || obstacleGrid[i][j]) {
+        return 0;
+      }
+      if (i == m - 1 && j == n - 1) {
+        return 1;
+      }
+      int &res = visited[i][j];
+      if (res != -1) {
+        return res;
+      }
+      return res = dfs(i + 1, j) + dfs(i, j + 1);
+    };
+    return dfs(0, 0);
+  }
+};
