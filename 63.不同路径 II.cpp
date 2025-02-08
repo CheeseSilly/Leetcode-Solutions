cd65199803da36606e3bef4fbe8bfc4bfafe09a1
Si11ycheese <si11ycheese6680@gmail.com>
2025-02-08 09:49:07 +0800

    update

--- 63.不同路径 II.cpp
+++ 63.不同路径 II.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
   int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
     vector<vector<int>> moves = {{0, 1}, {1, 0}};
@@ -25,5 +25,24 @@ public:
       return res = dfs(i + 1, j) + dfs(i, j + 1);
     };
     return dfs(0, 0);
+  }
+};
+
+class Solution2 {
+public:
+  int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+    vector<vector<int>> dp(m + 1, vector(n + 1, 0));
+    // dp[1][1]=1;
+    // dp[i+1][j+1]=dp[i+1][j]+dp[i][j+1]
+    dp[1][0] = 1;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (obstacleGrid[i][j] == 0)
+          dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1];
+      }
+    }
+    return dp[m][n];
   }
 };
