23b4994e43f2da7dd0b141272821e2b3946bfae1
Si11ycheese <si11ycheese6680@gmail.com>
2025-02-09 00:51:29 +0800

    update

---
+++ 80. 删除有序数组中的重复项 II.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    int n = nums.size();
+    int index = 2;
+    for (int i = 2; i < n; i++) {
+      if (nums[i] != nums[index - 2]) {
+        nums[index++] = nums[i];
+      }
+    }
+    return min(n, index);
+  }
+};
