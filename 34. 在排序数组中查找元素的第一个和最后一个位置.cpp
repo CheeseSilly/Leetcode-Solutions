f0c5c859e45c2bba96ffb043c0483a36239b7c70
Si11ycheese <si11ycheese6680@gmail.com>
2025-02-10 00:35:17 +0800

    update

---
+++ 34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> searchRange(vector<int> &nums, int target) {
+    int n = nums.size();
+    vector<int> ans(2, -1);
+    int flag = 0;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] == target) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 1) {
+      auto b = lower_bound(nums.begin(), nums.end(), target);
+      auto e = upper_bound(nums.begin(), nums.end(), target);
+      ans[0] = b - nums.begin();
+      ans[1] = e - nums.begin();
+    }
+    return ans;
+  }
+};
