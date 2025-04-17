8bf5eb68ac91ceba5325c0e21330af02c7adf1af
Si11ycheese <si11ycheese6680@gmail.com>
2025-04-17 12:30:47 +0800

    update

---
+++ 977. Squares of a Sorted Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> sortedSquares(vector<int> &nums) {
+    for (auto &i : nums) {
+      i = i * i;
+    }
+
+    int i = 0;
+    int j = nums.size() - 1;
+
+    vector<int> ans(nums.size(), 0);
+    int idx = nums.size() - 1;
+
+    while (i <= j) {
+      if (nums[i] < nums[j]) {
+        ans[idx] = nums[j];
+        j--;
+      } else if (nums[i] >= nums[j]) {
+        ans[idx] = nums[i];
+        i++;
+      }
+      idx -= 1;
+    }
+    return ans;
+  }
+};
