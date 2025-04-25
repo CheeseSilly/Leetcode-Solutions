e1b17f0455f290535ea9c0dd8c0452a6345bdb80
SillyCheese <hazard@SillyCheesedeMacBook-Air.local>
2025-04-25 16:09:43 +0800

    update

---
+++ 2845. Count of Interesting Subarrays.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  long long countInterestingSubarrays(vector<int> &nums, int modulo, int k) {
+    vector<int> sum(nums.size() + 1);
+    for (int i = 0; i < sum.size(); i++) {
+      sum[i + 1] = sum[i] + (nums[i] % modulo == k);
+    }
+
+    //(sum[j+1]-sum[i])%modulo==k%modulo
+    //(sum[j+1]-k)%modulo=sum[j]%modulo
+    unordered_map<int, int> um;
+    long long ans;
+    for (auto s : sum) {
+      if (s - k >= 0) {
+        ans += um[(s - k) % modulo];
+      }
+      um[s % modulo]++;
+    }
+    return ans;
+  }
+};
